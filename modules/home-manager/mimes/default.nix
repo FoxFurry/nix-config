5941ae26bedc64b6858db649c384fb3af57dfe41
FoxFurry <isacartur@gmail.com>
2024-01-10 00:01:11 +0200

    add mimeapps subconfig

---
+++ modules/home-manager/mimes/default.nix
@@ -0,0 +1,14 @@
+{}:
+
+{
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "vivaldi.desktop";
+      "x-scheme-handler/http" = "vivaldi.desktop";
+      "x-scheme-handler/https" = "vivaldi.desktop";
+      "x-scheme-handler/about" = "vivaldi.desktop";
+      "x-scheme-handler/unknown" = "vivaldi.desktop";
+    };
+  };
+}
