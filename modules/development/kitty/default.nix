cb907b0087736dd3077d2df95034f45d27a32ba1
FoxFurry <isacartur@gmail.com>
2024-01-15 16:08:06 +0200

    add alacritty config

--- modules/development/kitty/default.nix
+++ modules/development/kitty/default.nix
@@ -1,9 +1,13 @@
 { pkgs, ... }:
 
 {
-  programs.kitty = {
-    enable = true;
-  };
+#  programs.kitty = {
+#    enable = true;
+#  };
+
+  programs.alacritty.enable = true;
 
   xdg.configFile."kitty" = { source = ./../../../.config/kitty; recursive = true; };
+  xdg.configFile."alacritty" = { source = ./../../../.config/alacritty; recursive = true; };
+
 }
