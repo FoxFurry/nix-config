20a7b37cf7f90ba3d4e07b7719cb25af2ec74bfa
FoxFurry <isacartur@gmail.com>
2024-01-20 00:04:59 +0200

    add vs code fix

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }:
 
 {
-
+    programs.vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        catppuccin.catppuccin-vsc
+        catppuccin.catppuccin-vsc-icons
+      ];
+      userSettings = {
+        "files.autoSave" = "off";
+        "[nix]"."editor.tabSize" = 2;
+        "window.titleBarStyle" = "custom";
+      };
+    };
 }
