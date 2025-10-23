c16e2decf26f76f66a41d47969c7c8a4b369687a
FoxFurry <isacartur@gmail.com>
2025-10-24 00:00:30 +0300

    fix nvidia drivers

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
   security.rtkit.enable = true;
   
   networking.firewall = {
